c7ed603c19a9c21fd0a01d4ebbdc0136f0db17a9
pparksso <thgml5143@hanmail.net>
2023-07-25 23:26:11 +0900

    prettier, env 세팅

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,16 @@
-import express, {Express, Request, Response} from 'express'
+import express, { Express, Request, Response } from 'express';
+import cors, { CorsOptions } from 'cors';
 
 const app: Express = express();
 const port = 8000;
 
-app.get('/',(req: Request, res: Response)=> {
-    res.send('express server')
+// 배포 시 origin 수정 필요(해당 도메인으로)
+app.use(cors({ origin: true, credentials: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('express server');
 });
 
 app.listen(port, () => {
-    console.log(`server : http://localhost:${port}`);
-})
+  console.log(`server : http://localhost:${port}`);
+});
